f526d048963e3b8184e7efa3908a2890a25a5205
EvertonWallace <everton542@hotmail.com>
2025-01-26 20:59:51 -0300

    criacao da funcao migrate_data

--- migrador.py
+++ migrador.py
@@ -20,3 +20,31 @@ def transformar_excel_por_mapeamento(data, MAPEAMENTO_VALORES):
     except Exception as e:
         print(f"Erro ao transformar os dados: {e}")
         return data
+
+def migrate_data(excel_file_path, output_csv_path):
+    try:
+        client = MongoClient(MONGO_URI)
+        db = client[DATABASE_NAME]
+        collection = db[COLLECTION_NAME]
+        print("Conexão com MongoDB estabelecida com sucesso!")
+
+        data = pd.read_excel(excel_file_path)
+        print(f"Dados carregados com sucesso do arquivo: {excel_file_path}")
+
+        data = transformar_excel_por_mapeamento(data, MAPEAMENTO_VALORES)
+
+        data.to_csv(output_csv_path, index=False, encoding="utf-8")
+        print(f"Arquivo CSV criado com sucesso: {output_csv_path}")
+
+        data_dict = data.to_dict(orient="records")
+
+        if data_dict:
+            collection.insert_many(data_dict)
+            print(f"{len(data_dict)} registros inseridos com sucesso no MongoDB!")
+        else:
+            print("Nenhum dado encontrado para migrar.")
+    
+    except Exception as e:
+        print(f"Erro ao migrar os dados: {e}")
+    finally:
+        client.close()
